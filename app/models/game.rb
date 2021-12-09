84aea0386b4ef263fea99d245fa0215c4815d800
mattjmason <masonmj10@gmail.com>
2021-12-08 23:27:47 -0500

    model associations

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,4 @@
 class Game < ApplicationRecord
+    has_many :collections
+    has_many :users, through: :collections
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+    has_secure_password
+    validates :email, presence: true
+    validates :email, uniquness: true
+    validates :email, length: {minimum: 4}
+
+    has_many :collections
+    has_many :games, through: :collections
+    
 end
