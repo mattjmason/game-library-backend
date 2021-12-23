816eb93328635a72ad202df7782f32ba5d3a4077
mattjmason <masonmj10@gmail.com>
2021-12-23 09:19:49 -0500

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'pages#home'
+  resources :sessions, only: [:create]
+  resources :games
+
+  #USER ROUTES
+  get 'user/collections', to: 'users#collection'
+  delete '/user/collections/:id', to: 'users#destroy'
+  resources :users, only: [:create, :index, :destroy]
+
+  #SESSION ROUTES
+  get 'logged_in', to: 'sessions#logged_in'
+  delete 'logout', to: 'sessions#logout'
+
+  #GAME ROUTES
+  post '/games/:id', to: 'games#game_collection'
 end
